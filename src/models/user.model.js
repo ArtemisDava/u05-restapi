a36c4ba918a05ad54afe04dee08362434cc7aec9
Artemis Davakou <artemis.davakou@chasacademy.se>
2025-03-04 23:59:31 +0100

    feat: add user model

---
+++ src/models/user.model.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import { ingredientItemSchema, ingredientQuantitySchema } from "./ingredient.model";
+import { recipeItemSchema } from "./recipe.model";
+
+const userSchema = new mongoose.Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    apiKey: { type: String, required: true, unique: true }, // TO-DO
+    cart: [ingredientQuantitySchema],
+    favoriteRecipes: [recipeItemSchema],
+    allergies: [ingredientItemSchema],
+    isAdmin: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
+ 
+export default mongoose.model("User", userSchema);
