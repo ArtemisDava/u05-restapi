614e92178dc01ea63c5cce4e3f5c1c449fc9b897
Artemis Davakou <artemis.davakou@chasacademy.se>
2025-03-06 01:03:01 +0100

    refact: remove admin middleware from category routes

--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -1,16 +1,22 @@
 import express from "express";
-import CategoryController from "../controllers/category.controller.js";
-import isAdminMiddleware from "../middleware/admin.middleware.js";
+import categoryController from "../controllers/category.controller.js";
+// import isAdminMiddleware from "../middleware/admin.middleware.js";
 
 const router = express.Router();
 
 // Protected routes for authenticated admins
-router.put("/v1/admin/categories/:categoryID", isAdminMiddleware, CategoryController.updateCategoryByAdmin);
-router.delete("/v1/admin/categories/:categoryID", isAdminMiddleware, CategoryController.deleteCategoryByAdmin);
-router.post("/v1/admin/categories", isAdminMiddleware, CategoryController.createCategoryByAdmin);
+router.put(
+  "/v1/admin/categories/:categoryID",
+  categoryController.updateCategoryByAdmin
+);
+router.delete(
+  "/v1/admin/categories/:categoryID",
+  categoryController.deleteCategoryByAdmin
+);
+router.post("/v1/admin/categories", categoryController.createCategoryByAdmin);
 
 // Public routes
-router.get("/v1/categories/:categoryID", CategoryController.getCategoryById);
-router.get("/v1/categories", CategoryController.getAllCategories);
+router.get("/v1/categories/:categoryID", categoryController.getCategoryById);
+router.get("/v1/categories", categoryController.getAllCategories);
 
 export default router;
