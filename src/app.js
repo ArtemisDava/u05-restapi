d449dcf1297a55dc8c49148db3ea4d9d41cb8653
Artemis Davakou <artemis.davakou@chasacademy.se>
2025-03-27 10:01:17 +0100

    refact: remove commentted code

--- src/app.js
+++ src/app.js
@@ -1,15 +1,19 @@
 import express from "express";
 import morgan from "morgan";
 import dotenv from "dotenv";
-// import { specs, swaggerUI } from "./swagger.js";
+import cors from "cors";
+import { specs, swaggerUI } from "./swagger.js";
 import categoryRoutes from "./routes/category.routes.js";
-// import ingredientRoutes from "./routes/ingredient.routes.js";
-// import recipeRoutes from "./routes/recipe.routes.js";
+import ingredientRoutes from "./routes/ingredient.routes.js";
+import recipeRoutes from "./routes/recipe.routes.js";
 // import userRoutes from "./routes/user.routes.js";
 import { connectDB } from "./database.js";
 
 // Initialize Express app
 const app = express();
+
+// Enable CORS
+app.use(cors());
 
 // Load environment variables
 dotenv.config();
@@ -22,12 +26,12 @@ app.use(morgan("dev"));
 app.use(express.json());
 
 // Swagger API documentation
-// app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs));
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 
 // Routes
 app.use("/api", categoryRoutes);
-// app.use("/api", ingredientRoutes);
-// app.use("/api", recipeRoutes);
+app.use("/api", ingredientRoutes);
+app.use("/api", recipeRoutes);
 // app.use("/api", userRoutes);
 
 // Error handling middleware
