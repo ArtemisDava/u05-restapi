ebf3b38e87ab7c17ba33e6c68feb0aa14e144995
Artemis Davakou <artemis.davakou@chasacademy.se>
2025-03-27 10:11:53 +0100

    fix: syntax errors

--- src/models/utils/incrementalId.js
+++ src/models/utils/incrementalId.js
@@ -1,22 +1,12 @@
-function assignIncrementalId(schema) {
+import mongoose from "mongoose";
+
+export const assignIncrementalId = (schema) => {
   schema.pre("save", async function (next) {
-    if (this.id) {
-      return next();
+    if (this.isNew) {
+      const Model = mongoose.model(this.constructor.modelName);
+      const lastDoc = await Model.findOne().sort({ id: -1 });
+      this.id = lastDoc ? lastDoc.id + 1 : 1;
     }
-
-    // Find the document with the highest ID
-    const highestDoc = await this.constructor
-      .findOne({}, { id: 1 })
-      .sort({ id: -1 })
-      .limit(1);
-
-    // If there are no documents, start with ID 1
-    // Otherwise, increment the highest ID
-    this.id = highestDoc ? highestDoc.id + 1 : 1;
-
     next();
   });
-}
-
-// module.exports = { assignIncrementalId };
-export { assignIncrementalId };
+};
