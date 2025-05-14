330f856764af49b2289313c260ace33f7cf4482d
Artemis Davakou <artemis.davakou@chasacademy.se>
2025-05-14 10:42:15 +0200

    chore: fix format

--- src/controllers/recipe.controller.js
+++ src/controllers/recipe.controller.js
@@ -1,5 +1,12 @@
 import Recipe from "../models/recipe.model.js";
-import { checkIfExists, parseId, convertToCapitalizedLetters, sanitizeName, sanitizeDescription, convertToCapitalizedLetter } from "./utils/validators.js";
+import {
+  checkIfExists,
+  parseId,
+  convertToCapitalizedLetters,
+  sanitizeName,
+  sanitizeDescription,
+  convertToCapitalizedLetter,
+} from "./utils/validators.js";
 import errorHandler from "./utils/error.js";
 import filterFields from "./utils/transform-data.js";
 
@@ -16,7 +23,9 @@ const fields = [
 const RecipeController = {
   getRecipes: async (_, res) => {
     try {
-      let recipes = await Recipe.find({}).populate("ingredients.ingredient", "name").sort({ name: 1 });
+      let recipes = await Recipe.find({})
+        .populate("ingredients.ingredient", "name")
+        .sort({ name: 1 });
 
       recipes = filterFields(recipes, fields);
 
@@ -39,8 +48,10 @@ const RecipeController = {
     }
 
     try {
-      let recipe = await Recipe.findOne({ id: recipeId })
-      .populate("ingredients.ingredient", "name");
+      let recipe = await Recipe.findOne({ id: recipeId }).populate(
+        "ingredients.ingredient",
+        "name"
+      );
       if (!recipe) {
         return errorHandler(res, 404, `Recipe: '${recipeId}' not found`);
       }
@@ -61,26 +72,34 @@ const RecipeController = {
       const { name, ingredients, alternatives, instructions, category, image } =
         req.body;
 
-      if (!name || !instructions || !category || !ingredients || image) 
-        return errorHandler(res, 400, "Name, ingredients, instructions, image and category are required");
+      if (!name || !instructions || !category || !ingredients || !image)
+        return errorHandler(
+          res,
+          400,
+          "Name, ingredients, instructions, image and category are required"
+        );
 
       if (!Array.isArray(ingredients) || ingredients.length === 0)
-        return errorHandler(res, 400, "Ingredients must be an array and cannot be empty");
+        return errorHandler(
+          res,
+          400,
+          "Ingredients must be an array and cannot be empty"
+        );
 
       const nameValidation = sanitizeName(name, "Recipe name");
-      if (nameValidation.message) 
+      if (nameValidation.message)
         return errorHandler(res, 400, nameValidation.message);
-      
-      
 
       const transformedName = convertToCapitalizedLetters(name);
-      
 
       let exists = await checkIfExists(Recipe, { name: transformedName });
-      if (exists) 
-        return errorHandler(res, 400, `Recipe '${transformedName}' already exists`);
+      if (exists)
+        return errorHandler(
+          res,
+          400,
+          `Recipe '${transformedName}' already exists`
+        );
 
-      
       // Create new recipe
       const recipe = new Recipe({
         name: transformedName,
@@ -92,7 +111,7 @@ const RecipeController = {
       });
 
       try {
-      await recipe.save();
+        await recipe.save();
       } catch (error) {
         return errorHandler(res, 500, error.message);
       }
@@ -126,34 +145,39 @@ const RecipeController = {
         const nameValidation = sanitizeName(name, "Recipe name");
         if (nameValidation.message) {
           return errorHandler(res, 400, nameValidation.message);
-       }
-        
-
-        // Process name to be capitalized
-        const transformedName = convertToCapitalizedLetters(nameValidation.sanitized);
-
-        try {
-        const exists = await checkIfExists(Recipe, { 
-          name: transformedName, 
-          id: { $ne: recipeId } 
-        });
-        if (exists) {
-          return errorHandler(
-            res,
-            400,
-            `Recipe '${transformedName}' matches an existing recipe name!`
-          );
         }
 
-        recipe.name = transformedName;
-       } catch (error) {
-        return errorHandler(res, 500, error.message);
-       }
+        // Process name to be capitalized
+        const transformedName = convertToCapitalizedLetters(
+          nameValidation.sanitized
+        );
+
+        try {
+          const exists = await checkIfExists(Recipe, {
+            name: transformedName,
+            id: { $ne: recipeId },
+          });
+          if (exists) {
+            return errorHandler(
+              res,
+              400,
+              `Recipe '${transformedName}' matches an existing recipe name!`
+            );
+          }
+
+          recipe.name = transformedName;
+        } catch (error) {
+          return errorHandler(res, 500, error.message);
+        }
       }
 
       if (ingredients !== undefined) {
         if (!Array.isArray(ingredients) || ingredients.length === 0) {
-          return errorHandler(res, 400, "Ingredients must be an array and cannot be empty");
+          return errorHandler(
+            res,
+            400,
+            "Ingredients must be an array and cannot be empty"
+          );
         }
         recipe.ingredients = ingredients ?? recipe.ingredients;
       }
@@ -181,7 +205,7 @@ const RecipeController = {
       }
 
       try {
-      await recipe.save();
+        await recipe.save();
       } catch (error) {
         return errorHandler(res, 500, error.message);
       }
